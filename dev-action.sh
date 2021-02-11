6ac44157d5ce54f428609f22b5cd545cc060e6cb
Matthew Quinn <matt@mquinn.co.uk>
2021-02-11 14:49:45 +0000

    added mock server implementation

--- dev-action.sh
+++ dev-action.sh
@@ -3,4 +3,4 @@
 ACTION=$1
 
 # Dev script to apply running config to microk8s cluster
-microk8s kubectl $ACTION -f dep-locust-master.yaml -f svc-locust-service.yaml -f dep-locust-worker.yaml -f ing-locust-ui.yaml
+microk8s kubectl $ACTION -f dep-locust-master.yaml -f svc-locust-service.yaml -f svc-mock-server-service.yaml -f dep-locust-worker.yaml -f dep-mock-server.yaml -f ing-locust-ui.yaml
